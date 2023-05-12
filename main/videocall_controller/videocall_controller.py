93b74e290234a2a374820f4fa668a49b54322e69
justinfat <j7768775@gmail.com>
2023-05-12 17:43:22 +0800

    0512

--- main/videocall_controller/videocall_controller.py
+++ main/videocall_controller/videocall_controller.py
@@ -1,17 +1,40 @@
 import cv2
+import sys
 
-cap = cv2.VideoCapture(0)
+def webcam_capture(cam_id):
+    cap = cv2.VideoCapture(cam_id)
 
-while True:
-    ret, frame = cap.read()
+    print('Video width:',cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print('Video height:',cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print('Video fps:',cap.get(cv2.CAP_PROP_FPS))
 
-    if not ret:
-        break
+    #if cap.isOpened() == False:
+    #    cap.open()
 
-    cv2.imshow('Video', frame)
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # when read sucessfully, ret = True
+        frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        if not ret:
+            print('unable to read the video...')
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('webcam capture', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destoryAllWindows()
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    print(argv)
+    print('Opencv version:', cv2.__version__)
+
+    webcam_capture(0)
+
+if __name__ == '__main__':
+    sys.exit(main())
